04b10245d39f5946234fb393c9f78cdc1230859f
dakidinesh <dakidinesh321@gmail.com>
2025-09-04 15:52:51 -0700

    Day 1

--- Arrays_And_Hashing/217. Contains Duplicate.py
+++ Arrays_And_Hashing/217. Contains Duplicate.py
@@ -14,5 +14,21 @@
 #Input: nums = [1, 2, 3, 4]
 #Output: false
 
+#Solution 1
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(nums) != len(set(nums))  
+    
+#if length of set is less than length of list, it means there are duplicates in the list
 
+#Solution 2
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        a=set()
+        for i in nums:
+            if i in a:
+                return True
+            a.add(i)
+        return False
+    
 #if any element is already in the set, it means there are duplicates in the list
